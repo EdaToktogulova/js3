0ac78891a94c8735431329232cb0c793fd177386
Eda Toktogulova <edatoktogulova2003@gmail.com>
2021-12-27 13:23:55 +0600

    added new js file

---
+++ script.js
@@ -0,0 +1,129 @@
+// var message = prompt("What is your message?");
+// if (message === "") {
+//     message = "no message";
+// }
+// alert(message)
+
+// string "1991"
+// number 1991
+// boolean
+
+// var year = prompt("What year Kyrgyzstan gained independence?");
+// if (year === "1991") {
+//     alert ("Correct!");
+// }
+// else{
+//     alert("Incorrect!");
+// }
+
+// var example1 = "Hello" + "World";
+// console.log(example1);
+
+// var example1 = 100 + 100;
+// var example2 = "10" + "10";
+// var example3 = "200" + 200;
+
+// console.log(example1);
+
+// var age = prompt("How old are you?");
+// var year = 2020 - age;
+// console.log (year);
+
+// var year = prompt("What year Kyrgyzstan gained independence?");
+// if (year === "1991") {
+//     alert("Correct");
+// }
+// else {
+//     alert ("Incorrect");
+// }
+
+// var year = prompt("What year Kyrgyzstan gained independence?");
+// if (year === "year > 1991") {
+//     alert("This year is after independence year");
+// }
+// else if (year === "1991") {
+//     alert ("You got it!");
+// }
+// else {
+//     alert ("This year is before independence year");
+// }
+
+// var city = prompt ("Your city?");
+// if (city === "Karakol") {
+//     alert ("You live in Ysys-Kol");
+// }
+
+// else if (city === "Bishkek") {
+//     alert("You live in Chui");
+// }
+
+// else if (city === "Talas") {
+//     alert("You live in Talas");
+// }
+
+// else if (city === "Batken") {
+//     alert("You live in Batken");
+// }
+
+// else if (city === "Naryn") {
+//     alert("You live in Naryn");
+// }
+
+// else if (city === "Jalal-Abad") {
+//     alert("You live in Jalal-Abad");
+// }
+
+// else if (city === "Osh") {
+//     alert("You live in Osh");
+// }
+
+// else {
+//     alert("?");
+// }
+
+// var currentNumber = 0;
+//  var stopNumber = 150;
+
+//  while (currentNumber < stopNumber) {
+//     currentNumber = currentNumber + 1;
+//     console.log(currentNumber)
+//}
+
+//  var currentNumber = 0;
+//  var stopNumber = 150;
+
+//  while (currentNumber > stopNumber) {
+//     currentNumber = stopNumber - 1;
+//     console.log(currentNumber)
+//  }
+
+// for (var i=1; i<=10; i++) {
+//     console.log(i);
+// }
+
+//  var country = prompt("What is the ciuntry you want know capital of?");
+//  if (country === "USA") {
+//     alert ("Washington");
+//  }
+// else if (country === "Kyrgystan") {
+//     alert ("Bishkek");
+// }
+// else if (country === "Kazakstan") {
+//     alert ("Nur Sultan");
+// }
+// else {
+//     alert("We don't know that country");
+// }
+
+
+// for (var i=50; i>=20; i--) {
+//     console.log(i);
+// }
+
+// for (var i=100; i>=0; i--) {
+//     console.log(i);
+// }
+
+// for (var i= 50; i<=100; i++) {
+//     console.log(i)
+// }
